08e6a3309dfba3d3f346ef02eef3c36a6dbed57e
Nguyễn Quang Đức <nguyenquangduc@MacBook-Pro.local>
2019-04-13 23:56:45 +0700

    UI update and Add some charts

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from sklearn.ensemble import RandomForestRegressor
 from aiohttp import web
 import socketio
 import pickle
+import json
 import os
 
-if (not os.path.exists('markDataNature.sav') or not os.path.exists('markDataSocial.sav')):
+if (not os.path.exists('markDataNature.sav') or not os.path.exists('markDataSocial.sav') or not os.path.exists('meanMark.json')):
     os.system('python3 markPrediction.py')
 
 markNatureModel = pickle.load(open('markDataNature.sav', 'rb'))
 markSocialModel = pickle.load(open('markDataSocial.sav', 'rb'))
-
+with open('meanMark.json', encoding='utf8') as json_file:
+    meanMark = json.load(json_file)
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
@@ -34,21 +36,39 @@ async def timeElapsed():
         await sio.emit('timeElapsed', {'data': '%s : %s : %s' % (hour, minute, second)}, namespace='/home')
 
 async def mainPage(request):
-    with open('webMainPage.html') as f:
+    with open('webMainPage.html', encoding='utf8') as f:
         return web.Response(text=f.read(), content_type='text/html')
+
+@sio.on('debug', namespace='/home')
+async def debug(sid, data):
+    print(data)
 
 @sio.on('calMark', namespace='/home')
 async def calMark(sid, markSubmit):
     if (markSubmit[0] == 'nature'):
         markSubmit = np.array(markSubmit[1:]).reshape(1, -1)
         markPrediction = markNatureModel.predict(markSubmit)
+        markDiff = np.sum(markPrediction - meanMark['nature'])
     elif (markSubmit[0] == 'social'):
         markSubmit = np.array(markSubmit[1:]).reshape(1, -1)
         markPrediction = markSocialModel.predict(markSubmit)
+        markDiff = np.sum(markPrediction - meanMark['social'])
     else:
         markPrediction = [0]*len(subjects)
-    markPrediction = np.round(markPrediction, 1).tolist()
-    await sio.emit('markPrediction', {'data': '%s' % (','.join(str(x) for x in markPrediction))}, room = sid, namespace = '/home')
+        markDiff = -10
+
+    markPrediction = (np.round(markPrediction*4)/4).tolist()
+    await sio.emit('markPrediction', {'data': '%s' % (','.join(str(x) for x in markPrediction)), 'meanMark': meanMark}, room = sid, namespace = '/home')
+    if (markDiff >= 0 and markDiff < 5):
+        await sio.emit('passOrFail', {'data': "Bạn có khả năng đậu Đại học!"}, room=sid, namespace='/home')
+    elif (markDiff >= 5 and markDiff < 10):
+        await sio.emit('passOrFail', {'data': "Bạn có khả năng CAO đậu Đại học!"}, room=sid, namespace='/home')
+    elif (markDiff >= 10):
+        await sio.emit('passOrFail', {'data': "Bạn chắc chắn đậu Đại học!"}, room=sid, namespace='/home')
+    elif (markDiff >= -5 and markDiff < 0):
+        await sio.emit('passOrFail', {'data': "Bạn có nguy cơ không đậu Đại học!"}, room=sid, namespace='/home')
+    elif (markDiff < -5):
+        await sio.emit('passOrFail', {'data': "Bạn có nguy cơ CAO không đậu Đại học!"}, room=sid, namespace='/home')
 
 @sio.on('disconnect', namespace='/home')
 def test_disconnect(sid):
@@ -57,6 +77,5 @@ def test_disconnect(sid):
 app.router.add_static('/static', 'static')
 app.router.add_get('/', mainPage)
 
-#http://0.0.0.0:8080/result?subCom=nature&math0=0&math1=0&math2=0&literature0=0&literature1=0&literature2=0&english0=0&english1=0&english2=0&physics0=0&physics1=0&physics2=0&chemistry0=0&chemistry1=0&chemistry2=0&biology0=0&biology1=0&biology2=0&history0=0&history1=0&history2=0&geography0=0&geography1=0&geography2=0&civiceducation0=0&civiceducation1=0&civiceducation2=0
 sio.start_background_task(timeElapsed)
 web.run_app(app)
--- markPrediction.py
+++ markPrediction.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import csv
 import pickle
+import json
 
 numberOfTree = 250
 numberOfSubject = 9
@@ -12,6 +13,8 @@ numberOfYear = 3
 markData = []
 markDataNature = []
 markDataSocial = []
+subjects = ['math', 'literature', 'english', 'physics', 'chemistry', 'biology', 'history', 'geography', 'civiceducation']
+meanMark = {}
 
 # Covert .xlsx to .csv
 pd.read_excel('markData.xlsx', 'Sheet1', index_col=None).to_csv('markData.csv', encoding='utf-8')
@@ -22,14 +25,15 @@ with open('markData.csv', 'rt') as csvfile:
         markData.append(row)
 
 markData = np.array(markData)
-for line in markData[2:, 2:]:
+
+for line in markData[2:, 3:]:
     if (line[-1] != '0'):
         markDataSocial.append(line)
     else:
         markDataNature.append(line)
 
-markDataNature = np.array(markDataNature)
-markDataSocial = np.array(markDataSocial)
+markDataNature = np.array(markDataNature).astype(np.float32)
+markDataSocial = np.array(markDataSocial).astype(np.float32)
 
 # Train markDataNature Model
 # train, test= train_test_split(markDataNature, test_size=0.1)
@@ -39,6 +43,7 @@ markDataSocial = np.array(markDataSocial)
 # testLabel = test[:,numberOfSubject*numberOfYear:]
 trainData = markDataNature[:,:numberOfSubject*numberOfYear]
 trainLabel = markDataNature[:,numberOfSubject*numberOfYear:]
+meanMark['nature'] = (np.round(np.sum(trainLabel, axis = 0) * 4 / len(trainLabel)) / 4).tolist()
 clf = RandomForestRegressor(n_estimators  = numberOfTree)
 clf.fit(trainData, trainLabel)
 # Save Model
@@ -58,6 +63,7 @@ print('Nature Trained!')
 # testLabel = test[:,numberOfSubject*numberOfYear:]
 trainData = markDataSocial[:,:numberOfSubject*numberOfYear]
 trainLabel = markDataSocial[:,numberOfSubject*numberOfYear:]
+meanMark['social'] = (np.round(np.sum(trainLabel, axis = 0) * 4 / len(trainLabel)) / 4).tolist()
 clf = RandomForestRegressor(n_estimators  = numberOfTree)
 clf.fit(trainData, trainLabel)
 # Save Model
@@ -68,3 +74,8 @@ print('Social Trained!')
 #     print(clf.predict(testData[i].reshape(1,-1)))
 # print('Real Value: ')
 # print(testLabel)
+
+
+# Save meanMark into json
+with open('meanMark.json', "w", encoding='utf8') as json_file:
+        json.dump(meanMark, json_file, sort_keys=True, indent=4, ensure_ascii=False)
